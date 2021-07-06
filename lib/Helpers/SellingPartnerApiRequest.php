23dfae8fd33a1a346dbd8a78e23c122ca17f1598
liweib <liweib@sailvan.com>
2021-07-06 17:21:57 +0800

    add GB_VOEC

--- lib/Helpers/SellingPartnerApiRequest.php
+++ lib/Helpers/SellingPartnerApiRequest.php
@@ -138,8 +138,6 @@ trait SellingPartnerApiRequest
             } else {
                 $content = $responseBody->getContents();
                 if (!in_array($returnType, ['string', 'integer', 'bool'])) {
-                    $content = str_replace('"DeemedResellerCategory": IOSS', '"DeemedResellerCategory": "IOSS"', $content); // bug
-                    $content = str_replace('"DeemedResellerCategory": UOSS', '"DeemedResellerCategory": "UOSS"', $content); // bug
                     $content = json_decode($content);
                 }
             }
