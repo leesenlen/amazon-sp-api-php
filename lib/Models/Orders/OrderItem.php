b794d22f3f81fc1e5560cf6d05b72b8acee04d2b
liweib <liweib@sailvan.com>
2021-07-19 14:25:34 +0800

    OrderItem

--- lib/Models/Orders/OrderItem.php
+++ lib/Models/Orders/OrderItem.php
@@ -295,6 +295,7 @@ class OrderItem implements ModelInterface, ArrayAccess
     const DEEMED_RESELLER_CATEGORY_IOSS = 'IOSS';
     const DEEMED_RESELLER_CATEGORY_UOSS = 'UOSS';
     const DEEMED_RESELLER_CATEGORY_GB_VOEC = 'GB_VOEC';
+    const DEEMED_RESELLER_CATEGORY_CA_MPF = 'CA_MPF';
 
     /**
      * Gets allowable values of the enum.
@@ -307,6 +308,7 @@ class OrderItem implements ModelInterface, ArrayAccess
             self::DEEMED_RESELLER_CATEGORY_IOSS,
             self::DEEMED_RESELLER_CATEGORY_UOSS,
             self::DEEMED_RESELLER_CATEGORY_GB_VOEC,
+            self::DEEMED_RESELLER_CATEGORY_CA_MPF,
         ];
     }
 
