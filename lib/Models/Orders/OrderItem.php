d464fcd410e66fc2d0cbbc30d17bec8600404a76
jun <8739079+noblero@users.noreply.github.com>
2022-09-29 14:51:57 +0800

    Update OrderItem.php
    新增code：AU_VOEC

--- lib/Models/Orders/OrderItem.php
+++ lib/Models/Orders/OrderItem.php
@@ -303,7 +303,8 @@ class OrderItem implements ModelInterface, ArrayAccess
     const DEEMED_RESELLER_CATEGORY_CA_MPF = 'CA_MPF';
     const DEEMED_RESELLER_CATEGORY_NO_VOEC = 'NO_VOEC';
     const DEEMED_RESELLER_CATEGORY_NZ_VOEC = 'NZ_VOEC';
-
+    const DEEMED_RESELLER_CATEGORY_AU_VOEC = 'AU_VOEC';
+    
     /**
      * Gets allowable values of the enum.
      *
@@ -318,6 +319,7 @@ class OrderItem implements ModelInterface, ArrayAccess
             self::DEEMED_RESELLER_CATEGORY_CA_MPF,
             self::DEEMED_RESELLER_CATEGORY_NO_VOEC,
             self::DEEMED_RESELLER_CATEGORY_NZ_VOEC,
+            self::DEEMED_RESELLER_CATEGORY_AU_VOEC,
         ];
     }
 
