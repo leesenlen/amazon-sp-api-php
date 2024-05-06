1fd964fe20af978789e5cac1ca18ea1b05833b7f
李伟B <liweib@sailvan.com>
2024-05-06 14:18:19 +0800

    endpoint agent for ziniao

--- lib/SellingPartnerEndpoint.php
+++ lib/SellingPartnerEndpoint.php
@@ -7,10 +7,10 @@ class SellingPartnerEndpoint
     public static $EUROPE = 'https://sellingpartnerapi-eu.amazon.com';
     public static $FAR_EAST = 'https://sellingpartnerapi-fe.amazon.com';
     public static $NORTH_AMERICA = 'https://sellingpartnerapi-na.amazon.com';
-    public static $KUNIAO_EUROPE = 'https://sp-test.koniao.com/eu';
-    public static $KUNIAO_FAR_EAST = 'https://sp-test.koniao.com/fe';
-    public static $KUNIAO_NORTH_AMERICA = 'https://sp-test.koniao.com/na';
-    public static $ZINIAO_EUROPE = 'https://sbappstoreapi.ziniao.com/spapi-proxy/eu';
-    public static $ZINIAO_FAR_EAST = 'https://sbappstoreapi.ziniao.com/spapi-proxy/fe';
-    public static $ZINIAO_NORTH_AMERICA = 'https://sbappstoreapi.ziniao.com/spapi-proxy/na';
+    public static $KUNIAO_EUROPE = 'https://aliyunhk-proxy_sp-test.valsun.cn/eu';
+    public static $KUNIAO_FAR_EAST = 'https://aliyunhk-proxy_sp-test.valsun.cn/fe';
+    public static $KUNIAO_NORTH_AMERICA = 'https://aliyunhk-proxy_sp-test.valsun.cn/na';
+    public static $ZINIAO_EUROPE = 'https://aliyunhk-proxy_sbappstoreapi.valsun.cn/spapi-proxy/eu';
+    public static $ZINIAO_FAR_EAST = 'https://aliyunhk-proxy_sbappstoreapi.valsun.cn/spapi-proxy/fe';
+    public static $ZINIAO_NORTH_AMERICA = 'https://aliyunhk-proxy_sbappstoreapi.valsun.cn/spapi-proxy/na';
 }
